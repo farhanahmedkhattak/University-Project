65da0b8401ba14192e5a5c3580a344ae18779515
Mavii56 <127356982+Mavii56@users.noreply.github.com>
2026-09-20 14:17:26 +0500

    Complete build1.4

--- UniversityProject/src/main.jsx
+++ UniversityProject/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './AIMachineLearning.css'
+import AIMachineLearning from './AIMachineLearning.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AIMachineLearning />
   </StrictMode>,
 )
